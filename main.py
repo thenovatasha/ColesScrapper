4c65ee5377b0a68f8bd9dc334440b554d8545c24
Nova Tasha <thenovatasha@gmail.com>
2023-07-17 21:05:27 +1000

    Add multiple page scrolling functionality

--- main.py
+++ main.py
@@ -1,28 +1,62 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
+import time
 
-URL = 'https://www.coles.com.au/on-special'
-# go through all the pages. 
-# Find all products in url
-html_text = requests.get(URL).text
-soup = BeautifulSoup(html_text, 'lxml')
+def retrieve_data(html_text):
 
-# except the ones with no prices
-non_ads = soup.find_all('section', attrs={"data-testid":'product-tile'})
+    html_text = html_text.text
+    soup = BeautifulSoup(html_text, 'lxml')
 
-# parse through all tiles and store in zip
-all_names, all_prices = [], []
-for tile in non_ads:
+    # filter out the products with no prices
+    non_ads = soup.find_all('section', attrs={"data-testid":'product-tile'})
 
-    product_price = tile.find('span', class_ = 'price__value').text
-    product_name = tile.find('h2', class_ = 'product__title').text.rstrip()
-    all_names.append(product_name)
-    all_prices.append(product_price)
-    # for each product, get the link from the a tag.
-        # enter the link and get the product id.
+    # parse through all product tiles, retrieve info and store in lists
+    product_names, product_prices, product_links, product_codes = [], [],[],[]
+    for tile in non_ads:
 
-data = zip(all_names, all_prices)
-print(list(data))
+        
+        product_name = tile.find('h2', class_ = 'product__title').text.rstrip()
+        try:
+            product_price = tile.find('span', class_ = 'price__value').text
+        except AttributeError:
+            print(f'Check {product_name}, {page_num}')
+            continue
 
+        product_link ='coles.com.au' + tile.find('a', class_ = 'product__link').get("href")
+        product_code = product_link.split('-')[-1]
+        product_names.append(product_name)
+        product_prices.append(product_price)
+        product_links.append(product_link)
+        product_codes.append(product_code)
+        
+    # Store data in csv
+    data = list(zip(product_names, product_prices, product_codes, product_links))
+    if len(data) == 0:
+         return 0
+    else: 
+        return data
+    
 
+page_num = 1
+data = []
+while True:
+    base_url = 'https://www.coles.com.au/on-special'
+    URL = base_url + '?page=' + f'{page_num}'
+    html_text = requests.get(URL)
+    print(page_num)
+    print(f'Response code is : {html_text.status_code}')
+    data_found = retrieve_data(html_text)
+    if html_text.status_code != 200 or data_found == 0:
+        break
+    data += data_found
+    page_num += 1
+    time.sleep(2)
+    
 
+with open('coles_data.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['Product Name', 'Product Price', 'Product Code', 'Product Link'])
+        writer.writerows(data)
+
+print("Done successfully.")        
