f1e33f80d320cfc6e8b5525cd3a302d3a557daea
Nova Tasha <thenovatasha@gmail.com>
2023-07-20 00:29:58 +1000

    Add category function, tested.

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ import requests
 import csv
 import time
 
-def retrieve_data(html_text):
+def retrieve_data(html, category_name):
 
-    html_text = html_text.text
+    html_text = html.text
     soup = BeautifulSoup(html_text, 'lxml')
 
     # filter out the products with no prices
     non_ads = soup.find_all('section', attrs={"data-testid":'product-tile'})
 
     # parse through all product tiles, retrieve info and store in lists
-    product_names, product_prices, product_links, product_codes = [], [],[],[]
+    product_names, product_prices, product_links, product_codes, category_names = [], [], [], [], []
     for tile in non_ads:
 
         product_name = tile.find('h2', class_ = 'product__title').text.rstrip()
+        # ignore tiles which do not have a price or are out of stock
         try:
             product_price = tile.find('span', class_ = 'price__value').text
         except AttributeError:
@@ -28,9 +29,10 @@ def retrieve_data(html_text):
         product_prices.append(product_price)
         product_links.append(product_link)
         product_codes.append(product_code)
+        category_names.append(category_name)
         
     # Store data in csv
-    data = list(zip(product_names, product_prices, product_codes, product_links))
+    data = list(zip(product_names, product_prices, product_codes, product_links, category_names))
     if len(data) == 0:
          return 0
     else: 
@@ -45,28 +47,31 @@ category_tiles = soup_categories.find("ul", class_="sc-fc107670-0")
 category_links = []
 for tile in category_tiles:
      category_link = tile.find("a", attrs={'data-testid': 'category-card'}).get('href')
-     category_links.append(category_link)
+     category_name = tile.find("a", attrs={'data-testid': 'category-card'}).get('aria-label')
+
+
+     category_links.append((category_link, category_name))
 
 
 data = []
 BASE_URL = 'https://www.coles.com.au'
-for category in category_links:
+for category_link, category_name in category_links:
     page_num = 1
     while True:
         
-        URL = BASE_URL + category + '?page=' + f'{page_num}'
-        html_text = requests.get(URL)
+        URL = BASE_URL + category_link + '?page=' + f'{page_num}'
+        html = requests.get(URL)
         print(page_num)
-        print(category)
-        print(f'Response code is : {html_text.status_code}')
-        data_found = retrieve_data(html_text)
-        if html_text.status_code != 200 or data_found == 0:
+        print(category_name)
+        print(f'Response code is : {html.status_code}')
+        data_found = retrieve_data(html, category_name)
+        if html.status_code != 200 or data_found == 0:
             break
         data += data_found
         page_num += 1
         time.sleep(2)
     
-
+# write data as a csv format
 with open('coles_data.csv', 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(['Product Name', 'Product Price', 'Product Code', 'Product Link', 'Category'])
